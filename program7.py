34342c6de15bcfd30363465251bd62cdbcf7bf83
pandurangmanale <psmanale86@gmail.com>
2025-09-08 23:03:14 +0530

    Create program7.py
     Write a Python script using class, which has two methods get_String and print_String. 
    get_String accept a string from the user and print_String print the string in upper case.

---
+++ program7.py
@@ -0,0 +1,16 @@
+ Write a Python script using class, which has two methods get_String and print_String. 
+get_String accept a string from the user and print_String print the string in upper case. 
+
+
+Answer : 
+class MyClass:
+def Get_String(self):
+
+                                self.MyStr=input("Enter any String: ") 
+def Print_String(self):
+s=self.MyStr 
+ print("String in Upper Case: " , s.upper()) 
+# main body 
+Obj=MyClass() 
+Obj.Get_String() 
+Obj.Print_String() 
